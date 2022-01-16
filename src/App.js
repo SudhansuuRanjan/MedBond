e8fd4d6f45b4a467193a9429a052d0f92d379f97
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-01-16 23:18:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Started building <code>MedBond</code> App.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href=""
           rel="noopener noreferrer"
         >
-          Learn React
+          Med Bond
         </a>
       </header>
     </div>
