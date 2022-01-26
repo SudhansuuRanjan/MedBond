cc81db474e63ff8ffe5920b3c39abaf8e248416d
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-01-26 16:41:36 +0530

    testing

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const HomePage = () => {
     return (
